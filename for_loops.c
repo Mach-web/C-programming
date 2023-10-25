e2fb58367c5781ef908a50b5b2050da620136e1a
login <mkanderson771@gmail.com>
2023-10-25 09:23:45 +0300

    Evaluate odd numbers

--- for_loops.c
+++ for_loops.c
@@ -3,8 +3,8 @@
 int main(){
     int i;
     for(i = 1; i <= 20; i++){
-        if(i % 2 == 0){
-            printf("Even Number: %d\n", i);
+        if(i % 2 == 1){
+            printf("Odd Number: %d\n", i);
         }
     }
     return 0;
