89430109b8572d78f57f113a959ea178a875e196
login <mkanderson771@gmail.com>
2023-10-27 12:28:50 +0300

    Maximum and minimum number in an array

--- arrays.c
+++ arrays.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 int main(){
-int myNumber[]={100, 10, 25, 4, 428, 8, 56, 17, 33, 11, 29};
-int i = 0, max = myNumber[0];
+int myNumber[]={10, 10, 25, 4, 4, -8, 56, 17, 63, 11, 29};
+int i = 0, min = myNumber[0], max = myNumber[0]; 
 while (myNumber[i])
 {
     /* code */
-    if(max < myNumber[i]){
+    if(myNumber[i] < min){
+        min = myNumber[i];
+    }
+    if(myNumber[i] > max){
         max = myNumber[i];
     }
     i++;
 }
-printf("The maximum number is: %d\n", max);
+printf("The minimum number is: %d\n", min);
+printf(" The maximum number is: %d\n", max);
 return 0;
 }
